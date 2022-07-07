eca91da14054162ef39aa09c3cf832b8fdcf87b0
“Jim” <jimkernicky@gmail.com>
2022-07-07 17:52:05 -0400

    Small change

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -28,6 +28,7 @@ function UserSearch() {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
+        <p>Enter a GitHub user name or real name to search</p>
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
